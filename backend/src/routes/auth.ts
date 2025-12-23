7bc025a4f1c54fde7aee7b446e600dfc78963495
Laban <labanmusen32@gmail.com>
2025-12-23 10:11:11 +0300

    added / in sign_up

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -5,7 +5,7 @@ import { login, logout, sign_up } from "../controllers/auth.js";
 const router = express.Router();
 
 router.post("/login", login)
-router.post("sign_up", sign_up)
+router.post("/sign_up", sign_up)
 router.post("/logout", logout)
 
 
