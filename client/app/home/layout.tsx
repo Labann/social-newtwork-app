e2b839b1696baaefa687e9bce2fb82c4c6ab81f8
Laban <labanmusen32@gmail.com>
2026-01-16 14:33:01 +0300

    create post skeleton done

--- client/app/home/layout.tsx
+++ client/app/home/layout.tsx
@@ -15,7 +15,7 @@ const HomeLayout = ({children}: {
         <Navbar/>
         <div className='grid md:grid-cols-12 gap-4 p-4 max-w-7xl mx-auto'>
             <SideBar/>
-            <div className='shady-background col-span-7 p-3'>
+            <div className='col-span-7 p-3'>
               {children}
             </div>
             <RightBar/> 
--- client/components/CreatePost.tsx
+++ client/components/CreatePost.tsx
@@ -6,15 +6,15 @@ import { FaRegPlayCircle } from "react-icons/fa";
 import { IoDocumentOutline } from "react-icons/io5";
 const CreatePost = () => {
   return (
-    <form className='shady-background flex text-primary items-center space-x-3 p-3 rounded-md border-black/30 border'>
-        <CgProfile className='rounded-full'/>
+    <form className='shady-background flex text-[#9ca3af] items-center space-x-2 p-3 rounded-md border-black/30 border-2 shadow-md'>
+        <CgProfile size={"2em"} className='rounded-full'/>
         <input type="text" placeholder='Start a post' 
-            className='w-full border-0 bg-transparent'
+            className='w-full border-0 p-2 bg-transparent'
         />
-        <div className="flex items-center space-x-2">
-            <FaImage size={".7em"}/>
-            <FaRegPlayCircle size={".7em"}/>
-            <IoDocumentOutline size={".7em"}/>
+        <div className="flex items-center text-black/50 space-x-3 font-extrabold">
+            <FaImage size={".9em"} className='cursor-pointer'/>
+            <FaRegPlayCircle size={".9em"} className='cursor-pointer'/>
+            <IoDocumentOutline size={".9em"} className='cursor-pointer'/>
         </div>
     </form>
   )
